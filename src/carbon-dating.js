792b36c979bdd39d3c0eabb4cfb67f44f65d9f88
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-04-10 18:46:11 +0300

    cats, carbon, dream team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -13,13 +13,14 @@ const HALF_LIFE_PERIOD = 5730;
  *
  * @example
  * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
+ 
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(sampleActivity > 0 && sampleActivity < 15 && typeof sampleActivity === 'string') {
+    return Math.trunc(Math.log((sampleActivity/MODERN_ACTIVITY)) / (0.693/ -5730) + 1);
+  } else return false
+
 }
 
 module.exports = {
