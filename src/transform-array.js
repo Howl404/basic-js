b574091561b587202ce64ffd3032f84bbd2c767b
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-04-16 16:56:38 +0300

    1

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,51 +14,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  // if(Array.isArray(arr) === false) {
-  //   throw new Error("\'arr\' parameter must be an instance of the Array!")
+  if(Array.isArray(arr) === false) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!")
     
-  // }
-  // let array = arr.map((x) => x)
-  // for(let i = 0; i < array.length; i++) {
-  //   switch (array[i]) {
-  //     case "--discard-prev":
-  //       if(array[i - 1] !== undefined){
-  //         array.splice(i - 1, 2)
-  //       } else array.splice(i, 1)
-  //       i = 0;
-  //     break;
-  //     case "--discard-next":
-  //       if(array[i + 1] !== undefined){
-  //         array.splice(i, 2)
-  //       } else array.splice(i, 1)
-  //       i = 0;
-  //     break;
-  //     case "--double-next":
-  //       if(arr[i + 1] !== array[i + 1]) {
-  //         array.splice(i, 1)
-  //         break;
-  //       }
-  //       if(array[i + 1] !== undefined) {
-  //         array[i] = array[i + 1]
-  //       } else array.splice(i, 1)
-  //       i = 0;
-  //     break;
-  //     case "--double-prev":
-  //       console.log(1);
-  //       console.log(arr[i - 1]);
-  //       console.log(array[i]);
-  //       if(arr[i - 1] !== array[i]) {
-  //         array.splice(i, 1)
-  //         break;
-  //       }
-  //       if(array[i - 1] !== undefined) {
-  //         array[i] = array[i - 1]
-  //       } else array.splice(i, 1)
-  //       i = 0;
-  //     break;
-  //   }
-  // }
-  // return array;
+  }
+  let array = arr.map((x) => x)
+  for(let i = 0; i < array.length; i++) {
+    switch (array[i]) {
+      case "--discard-prev":
+        if(array[i - 1] !== undefined){
+          array.splice(i - 1, 2)
+        } else array.splice(i, 1)
+        i = 0;
+      break;
+      case "--discard-next":
+        if(array[i + 1] !== undefined){
+          array.splice(i, 2)
+        } else array.splice(i, 1)
+        i = 0;
+      break;
+      case "--double-next":
+        if(arr[i + 1] !== array[i + 1]) {
+          array.splice(i, 1)
+          break;
+        }
+        if(array[i + 1] !== undefined) {
+          array[i] = array[i + 1]
+        } else array.splice(i, 1)
+        i = 0;
+      break;
+      case "--double-prev":
+        console.log(1);
+        console.log(arr[i - 1]);
+        console.log(array[i]);
+        if(arr[i - 1] !== array[i]) {
+          array.splice(i, 1)
+          break;
+        }
+        if(array[i - 1] !== undefined) {
+          array[i] = array[i - 1]
+        } else array.splice(i, 1)
+        i = 0;
+      break;
+    }
+  }
+  return array;
 }
 console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
 module.exports = {
